9d37ec8969fd5d0183fdc52d3eb56d621f365e52
seasia.developers2 <seasia.developers2@gmail.com>
2022-06-24 18:33:10 +0530

    delete

--- src/app/posts/post-list/state/post.reducer.ts
+++ src/app/posts/post-list/state/post.reducer.ts
@@ -1,15 +1,38 @@
 import { createReducer, on } from "@ngrx/store"
 import { initialState } from "./../../../posts/post-list/state/post.state";
-import { addPost } from "./post.action";
+import { addPost, deletePost, updatePost } from "./post.action";
 
-const _postsReducer = createReducer(initialState,on(addPost,(state,action)=>{
+const _postsReducer = createReducer(initialState,
+    on(addPost,(state,action)=>{
     let post = {...action.post};
     post.id = (state.posts.length +1).toString();
     return {
         ...state,
         posts:[...state.posts,post],
     }
-}))
+}),
+on(updatePost,(state,action)=>{
+    console.log(state,"state")
+    const updatedPosts = state.posts.map(post=>{
+        console.log(post,"post")
+        return action.post.id === post.id ? action.post : post;
+    });
+    return {
+        ...state,
+        posts:updatedPosts,
+    }
+}),
+on(deletePost,(state,{id})=>{
+    const updatedPosts =state.posts.filter((post)=>{
+return post.id != id;
+    })
+    return {
+        ...state,
+        posts:updatedPosts,
+    }
+})
+
+)
 
 export function postsReducer(state:any,action:any){
     return _postsReducer(state,action)
--- src/app/posts/post-list/state/post.selector.ts
+++ src/app/posts/post-list/state/post.selector.ts
@@ -1,13 +1,18 @@
-import { createFeatureSelector, createSelector } from "@ngrx/store";
-import { PostsState } from "./post.state";
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { PostsState } from './post.state';
 
-  const getPostsState = createFeatureSelector<PostsState>('posts');
+const getPostsState = createFeatureSelector<PostsState>('posts');
 
-export const getPosts = createSelector(getPostsState,(state) => {
-    return state.posts;
-})
-export const getPostById = createSelector(getPostsState,(state:any,props:any)=>{
-    return state.posts.find((post:any)=>{
-        post.id===props.id
-    })
-})
+export const getPosts = createSelector(getPostsState, (state) => {
+  return state.posts;
+});
+export const getPostById = createSelector(
+  getPostsState,
+  (state: any, props: any) => {
+    return state.posts.find((post: any) => {
+        console.log(post,"post")
+       const postid= post.id === props.id
+       console.log(postid,"postid")
+      });
+  }
+);
